07af1e14a46b3245e2c863a881f38c955999bc47
SHOBHIT SHRIVASTAVA <111689849+Shobhit51@users.noreply.github.com>
2024-05-09 20:49:54 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <footer>
+        <p>
+          <a
+            href="https://github.com/Shobhit51/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          🚀
+        </p>
+      </footer>
+    </div>
+  );
+}
